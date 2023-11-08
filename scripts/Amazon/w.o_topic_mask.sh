fba9e1dee85fb55769e21712f942397fdfc695ee
hieuman <hieuman2708@gmail.com>
2023-11-08 14:50:12 -0800

    add: topic_mask ablation

---
+++ scripts/Amazon/w.o_topic_mask.sh
@@ -0,0 +1,1 @@
+python ERLAS/main.py --dataset_name Amazon --token_max_length 128 --index_by_BM25 --index_by_dense_retriever --BM25_percentage 0.5 --dense_percentage 0.5 --gpus 2 --use_gc --gc_minibatch_size 16 --learning_rate 2e-5 --learning_rate_scaling --num_epoch 2 --do_learn --experiment_id Amazon --version w.o_topic_mask
